7fe1afa1de9363e2e126763cab4d204bc9b16953
Shalom Steinbach <shalomss525@gmail.com>
2021-05-24 22:23:48 +0300

    enable users to supply a custom rollup.config.js

--- example/.eleventy.js
+++ example/.eleventy.js
@@ -1,17 +1,12 @@
 const embedSvelte = require('../');
 const sveltePreprocess = require('svelte-preprocess'); // Optional
-const { terser } = require('rollup-plugin-terser'); // Optional
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(embedSvelte, {
 		// Directory that hosts your *.svelte component files. (Optional)
 		svelteDir: './svelte',
 		// Options that you may pass to rollup-plugin-svelte. (Optional)
-		rollupPluginSvelteOptions: { preprocess: sveltePreprocess() },
-		// Array of Rollup input plugins. (Optional)
-		rollupInputPlugins: [],
-		// Array of Rollup output plugins. (Optional)
-		rollupOutputPlugins: [terser()]
+		rollupPluginSvelteOptions: { preprocess: sveltePreprocess() }
 	});
 
 	return {
